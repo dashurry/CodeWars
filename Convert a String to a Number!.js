c46b15e0134b37adcd79dcd19d30b9f465b91cb8
Mauro Frappietro <45306301+dashurry@users.noreply.github.com>
2022-12-27 10:58:42 +0100

    Create Convert a String to a Number!.js

---
+++ Convert a String to a Number!.js
@@ -0,0 +1,4 @@
+const stringToNumber = function(str) {
+  return parseInt(str);
+}
+// The parseInt function is a built-in JavaScript function that can be used to convert a string representation of a number (either an integer or a float) into an integer.
