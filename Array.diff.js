63748fc7d667793950ceb83cba1b74150c2dee48
Mauro Frappietro <45306301+dashurry@users.noreply.github.com>
2022-12-28 17:42:20 +0100

    Update Array.diff.js

--- Array.diff.js
+++ Array.diff.js
@@ -1,4 +1,3 @@
-```
 function arrayDiff(a, b) {
   // Create a Set from the second list to make it easier to check if a value
   // is present in that list.
@@ -11,4 +10,3 @@ function arrayDiff(a, b) {
 // creating a Set from the second list, which is a data structure that allows you to quickly check if a value is present in the set. 
 // Then, it uses the Array.filter() method to remove all values from the first list that are present in the second list. 
 // Finally, it returns the resulting array.
-```
